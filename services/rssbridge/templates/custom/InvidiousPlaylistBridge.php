f4e948efe867d28ebf3e40d64b010f81dbb6ceec
Jakub Filo <jakub@filo.dev>
2023-11-04 20:30:14 +0100

    Auto download Invidious playlist

---
+++ services/rssbridge/templates/custom/InvidiousPlaylistBridge.php
@@ -0,0 +1,50 @@
+<?php
+class InvidiousPlaylistBridge extends FeedExpander
+{
+
+  const MAINTAINER = 'No maintainer';
+  const NAME = 'Invidious Playlist bridge';
+  const URI = '';
+  const DESCRIPTION = 'No description provided';
+  const CACHE_TIMEOUT = 3600;
+
+  const PARAMETERS = [[
+    'invidious_host' => [
+      'name' => 'Invidious Host URL',
+      'required' => true
+    ],
+    'playlist_id' => [
+      'name' => 'Invidious Playlist ID',
+      'required' => true
+    ]
+  ]];
+
+  public function collectData()
+  {
+    $videos = [];
+    $page = 1;
+
+    while (true) {
+      $playlist_url = $this->getInput('invidious_host') . '/api/v1/playlists/' . $this->getInput('playlist_id') . '?page=' . $page++;
+      $playlist = json_decode(getContents($playlist_url), true);
+      $current_page_videos = $playlist['videos'];
+      if (count($current_page_videos) == 0) {
+        break;
+      }
+      $videos = array_merge($videos, $current_page_videos);
+    }
+
+    foreach ($videos as $video) {
+      $this->items[] = $this->parseItem($video);
+    }
+  }
+
+  protected function parseItem($feedItem)
+  {
+    $item = [];
+    $item['uri'] = "https://youtube.com/watch?v=" . $feedItem['videoId'];
+    $item['content'] = '';
+
+    return $item;
+  }
+}
